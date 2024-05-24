28fa2780da143bb077156a858b6f0025f5603cb4
Curtis <chuebner@pop-os.localdomain>
2024-05-24 06:25:56 +0100

    notebook for paper and better setup

--- setup.py
+++ setup.py
@@ -5,11 +5,20 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=[
-        "torch"
+        "torch",
     ],
+    extras_require={ 'demos': [
+            "jupyterlab",
+            "matplotlib",
+            "seaborn",
+            "numpy",
+            "x-transformers"
+        ]
+    },
     entry_points={
         'console_scripts': [
             'sampling_demo=demos.sampling_demo:main',
         ],
     },
 )
+
